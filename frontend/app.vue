8e01aa3a55e37f0cf17f4eedc70582b75e7fce70
abukaj <wulfrykpierozek@gmail.com>
2024-11-27 20:28:33 +0100

    init

---
+++ frontend/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <Header />
+    </q-header>
+
+    <q-page-container>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </q-page-container>
+
+    <!-- <q-footer>
+      <div class="text-center">Footer Content</div>
+    </q-footer> -->
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+  import Header from './src/components/Header.vue'; 
+</script>
+
+<style scoped>
+  /* Style dla aplikacji */
+</style>
---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-drawer v-if="rightDrawerOpen" side="right" overlay behavior="mobile">
+    <!-- drawer content -->
+  </q-drawer>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  rightDrawerOpen: boolean
+}>()
+// const props = defineProps(['rightDrawerOpen'])
+</script>
