bce26eb43261ba5cb0dea4acda6d88e2025b6246
abukaj <wulfrykpierozek@gmail.com>
2025-02-10 18:45:29 +0100

    changes in dependecies

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -16,15 +16,6 @@ from .server_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # Application definition
 
 INSTALLED_APPS = [
